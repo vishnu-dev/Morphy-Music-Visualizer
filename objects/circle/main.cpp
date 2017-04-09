c79dc0f9043bb5bbec69c1b69426424d85a340a0
Himanshu4746 <himanshu4746@gmail.com>
2017-04-09 18:00:51 +0530

    objects

---
+++ objects/circle/main.cpp
@@ -0,0 +1,155 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include<stdio.h>
+#include <stdio.h>
+#include<cmath>
+
+using namespace std;
+
+float r=20.0;  //circle "r"
+float d=0.8;     //cuboid width/2
+float deg=30.0;
+/* GLUT callback Handlers */
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //gluPerspective(View_angle, Aspect_ratio, Near, Far);
+    gluPerspective(60,(float)w/(float)h, 1, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //clears display with buffer color & depth values set in init()
+    //circle
+    glLoadIdentity();  //Loads identity matrix for each iteration of display
+    glTranslatef(0,0,-50.0);  //Translation motion along(x,y,z) axis
+    //rotation after translation (order matters)
+    glRotatef(deg,1,0,0);  //(degree, x,y,z);
+    // deg+=1.5;
+    glPointSize(10.0);
+    glColor3f(1,0,1);
+    float deg=0.0;
+    for(int i=0;i<64;i++)
+    {
+        float x=r*cos(deg*(3.14/180)),y=rand()%10,z=r*sin(deg*(3.14/180));
+        // if(y<3){
+        //     glColor3f(0,1,0);
+        // }
+        // else if(y<6){
+        //     glColor3f(0,0,1);
+        // }
+        // else{
+        //     glColor3f(1,0,0);
+        // }
+
+        glBegin(GL_QUADS);
+        //top
+        glColor3f(1,1,0);
+        glVertex3f(x-d,y,z+d); //-x,y,z
+        glColor3f(1,1,1);
+        glVertex3f(x+d,y,z+d); //x,y,z
+        glColor3f(1,0,1);
+        glVertex3f(x+d,y,z-d); //x,y,-z
+        glColor3f(1,0,0);
+        glVertex3f(x-d,y,z-d); //-x,y,-z
+
+        //bottom
+        glColor3f(0,1,0);
+        glVertex3f(x-d,0,z+d); //-x,y=0,z
+        glColor3f(0,1,1);
+        glVertex3f(x+d,0,z+d); //x,y=0,z
+        glColor3f(0,0,1);
+        glVertex3f(x+d,0,z-d); //x,y=0,-z
+        glColor3f(0,1,1);
+        glVertex3f(x-d,0,z-d); //-x,y=0,-z
+
+        //left
+        glColor3f(0,1,0);
+        glVertex3f(x-d,0,z+d); //-x,y=0,z
+        glColor3f(0,1,1);
+        glVertex3f(x-d,0,z-d); //-x,y=0,-z
+        glColor3f(1,0,0);
+        glVertex3f(x-d,y,z-d); //-x,y,-z
+        glColor3f(1,1,0);
+        glVertex3f(x-d,y,z+d); //-x,y,z
+
+        //right
+        glColor3f(0,1,1);
+        glVertex3f(x+d,0,z+d); //x,y=0,z
+        glColor3f(0,0,1);
+        glVertex3f(x+d,0,z-d); //x,y=0,-z
+        glColor3f(1,0,1);
+        glVertex3f(x+d,y,z-d); //x,y,-z
+        glColor3f(1,1,1);
+        glVertex3f(x+d,y,z+d); //x,y,z
+
+        //front
+        glColor3f(0,1,1);
+        glVertex3f(x+d,0,z+d); //x,y=0,z
+        glColor3f(1,1,1);
+        glVertex3f(x+d,y,z+d); //x,y,z
+        glColor3f(1,1,0);
+        glVertex3f(x-d,y,z+d); //-x,y,z
+        glColor3f(0,1,0);
+        glVertex3f(x-d,0,z+d); //-x,y=0,z
+
+        //back
+        glColor3f(0,0,1);
+        glVertex3f(x+d,0,z-d); //x,y=0,z
+        glColor3f(1,0,1);
+        glVertex3f(x+d,y,z-d); //x,y,z
+        glColor3f(1,0,0);
+        glVertex3f(x-d,y,z-d); //-x,y,z
+        glColor3f(0,1,1);
+        glVertex3f(x-d,0,z-d); //-x,y=0,z
+        glEnd();
+        deg+=5.625;
+    }
+    Sleep(100);
+    glutSwapBuffers();
+}
+
+//static void idle(void)
+//{
+//    glutPostRedisplay();
+//}
+
+void init()
+{
+    //sets color buffer bit
+    glClearColor(0.0,0.0,0.0,0.0);
+    glEnable(GL_DEPTH_TEST);  //enables DEPTH_TEST
+    glDepthFunc(GL_LEQUAL);   //Lesser depth & EQUAL depth valued objects displayed in the front
+    //Do anti alias
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POLYGON_SMOOTH);
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(10,10);
+    glutCreateWindow("Morphy");
+
+    init();
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+
+    return 0;
+}
